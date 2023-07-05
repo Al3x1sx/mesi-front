e21e27777f17bfe80593e56dc794e839e576248b
aleski.menard <aleski.menard@gmail.com>
2023-07-05 10:48:55 +0200

    correct style with no data

--- src/components/pages/species/species.component.ts
+++ src/components/pages/species/species.component.ts
@@ -7,16 +7,14 @@ import {Component} from "@angular/core";
 })
 export class SpeciesComponent {
 species: any[] = [
-  {id: 1, name: 'testA'},
-  {id: 1, name: 'testA'},
-  {id: 1, name: 'testA'},
-  {id: 1, name: 'testA'},
-  {id: 1, name: 'testA'},
-  {id: 1, name: 'testA'},
-  {id: 1, name: 'testA'},
-  {id: 1, name: 'testA'},
-  {id: 1, name: 'testA'},
-  {id: 1, name: 'testA'},
-  {id: 1, name: 'testA'}
-];
+  {id: 1, name: 'Tigre de Sunda'},
+  {id: 1, name: 'Elephant de Sumatra'},
+  {id: 1, name: 'Orang-outan de Bornéo'},
+  {id: 1, name: 'Gorille des plaines orientales'},
+  {id: 1, name: 'Marsoin aptère du Yangtsé'},
+  {id: 1, name: 'Rinocéros de Sumatra'},
+  {id: 1, name: 'Elephant de forêt africain'},
+  {id: 1, name: 'Gorille du fleuve Cross'},
+  {id: 1, name: 'Vaquita'},
+  {id: 1, name: 'Saola'}];
 }
